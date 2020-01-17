12912ec99d4b95975a532482fc02756673e3638b
The Awesome Alex <lordpizza11@gmail.com>
2020-01-17 22:47:29 +0000

    worked on next and iterate function. Added print points function. Added example points

--- wave_simulation1.py
+++ wave_simulation1.py
@@ -1,14 +1,25 @@
 from wave_point import Point
 
 def next(prev_points,time_passed):
-    new_points = []
-    for p in prev_points:
+    new_points = prev_points
+    for p in new_points:
         x,y = p.coords[0], p.coords[1]
         new_x,new_y = x + p.vel[0]*time_passed, y + p.vel[1]*time_passed
         p.coords = [new_x,new_y]
+    return new_points
 
 def iterate(start_points,n_iterations,time_between):
     points = start_points
     for i in range(0,n_iterations):
         points = next(points,time_between)
     return points
+
+def print_points(points):
+    for p in points:
+        print("X:",p.coords[0],"Y:",p.coords[1],"XV:",p.vel[0],"YV:",p.vel[1])
+
+points = [Point(0,0,1,0),Point(0,1,1,0),Point(0,2,1,0),Point(0,3,1,0)]
+
+points = iterate(points,4,1)
+
+print_points(points)
