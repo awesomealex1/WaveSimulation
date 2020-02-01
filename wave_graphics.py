2a1039cfd73815f35649bf6935d3087ebd56b5b6
The Awesome Alex <lordpizza11@gmail.com>
2020-02-01 16:52:01 +0000

    Changed graphics module to implement Graph class. Made it possible to see animated points.

--- wave_graphics.py
+++ wave_graphics.py
@@ -1,24 +1,19 @@
 import graphics
 
-class my_graphics:
-    def makewin():          # Makes Window
-        global win 
-        win = graphics.GraphWin("Wave Simulation", 500, 500)
+class Graph():
 
-    def array(points):      # Draws array of points
-        for p in points:
-            point = graphics.Point(p.coords[0], p.coords[1])
-            point.draw(win)
+    def __init__(self, points):
+        self.points = points
+        self.graphics_points = [graphics.Point(p.coords[0], p.coords[1]) for p in self.points]
+        self.win = graphics.GraphWin("Wave Simulation", 500, 500)
+        for p in self.graphics_points:
+            p.draw(self.win)
 
-    def point(point):       # Draws single point
-            p = graphics.Point(point.coords[0], point.coords[1])
-            p.draw(win)
+    def update_graph(self, new_points):
+        for i in range(0,len(self.graphics_points)):
+            self.graphics_points[i].move(new_points[i].coords[0]-self.graphics_points[i].getX(),new_points[i].coords[1]-self.graphics_points[i].getY())
+            print(new_points[i].coords[0]-self.points[i].coords[0],new_points[i].coords[1]-self.points[i].coords[1])
+        self.points = new_points
 
-    def point_xy(x, y):     # Draws single point given only x, y
-        p = graphics.Point(x, y)
-        p.draw(win)
-
-    def closewin():         # Closes Window
-        win.getMouse()
-        win.close
-    
+    def close_graph(self):
+        self.win.close()    
--- wave_simulation1.py
+++ wave_simulation1.py
@@ -1,5 +1,6 @@
 from wave_point import Point
-from wave_graphics import my_graphics
+from wave_graphics import Graph
+import time
 
 def next(prev_points,time_passed):
     new_points = prev_points
@@ -9,24 +10,24 @@ def next(prev_points,time_passed):
         p.coords = [new_x,new_y]
     return new_points
 
-def iterate(start_points,n_iterations,time_between):
+def iterate(start_points,n_iterations,time_between,graph_time_between,graph):
     points = start_points
     for i in range(0,n_iterations):
         points = next(points,time_between)
+        time.sleep(graph_time_between)
+        graph.update_graph(points)
     return points
 
 def print_points(points):
     for p in points:
         print("X:",p.coords[0],"Y:",p.coords[1],"XV:",p.vel[0],"YV:",p.vel[1])
 
-my_graphics.makewin()
+
 
 points = [Point(0,0,1,0),Point(0,10,1,0),Point(0,20,1,0),Point(0,30,1,0)]
 
-points = iterate(points,100,1)
+g = Graph(points)
 
-print_points(points)
+points = iterate(points,10,10,1,g)
 
-my_graphics.array(points)
-my_graphics.closewin()
-
+g.close_graph()
